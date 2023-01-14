a88bc32b62ab85e9df08665030fd2f40e89c5eb8
ceneva <v.peklo.korolya@mail.ru>
2023-01-14 13:48:41 +0300

    + score, lives and game success

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 const canvas = document.querySelector('#myCanvas')
 const ctx = canvas.getContext('2d')
+
 const paddleHeight = 10
 const paddleWidth = 75
+
 const brickRowCount = 3
 const brickColumnCount = 5
 const brickWidth = 75
@@ -15,29 +17,32 @@ const brickOffsetLeft = 30
 let rightPressed = false
 let leftPressed = false
 
-let x = canvas.width/2
-let y = canvas.height-30
-let paddleX = (canvas.width-paddleWidth)/2
+let x = canvas.width / 2
+let y = canvas.height - 30
+let paddleX = (canvas.width - paddleWidth) / 2
 
 let dx = 2
 let dy = -2
 
+let score = 0
+let lives = 3
+
 const ballRadius = 10
 
 let bricks = []
-for(let column = 0; column < brickColumnCount; column++) {
+for (let column = 0; column < brickColumnCount; column++) {
   bricks[column] = []
-  for(let row = 0; row < brickRowCount; row++) {
-    bricks[column][row] = { x: 0, y: 0, status: 1 }
+  for (let row = 0; row < brickRowCount; row++) {
+    bricks[column][row] = {x: 0, y: 0, status: 1}
   }
 }
 
 function drawBricks() {
-  for(let column = 0; column < brickColumnCount; column++) {
-    for(let row = 0; row < brickRowCount; row++) {
-      if(bricks[column][row].status === 1) {
-        let brickX = (column*(brickWidth+brickPadding))+brickOffsetLeft;
-        let brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
+  for (let column = 0; column < brickColumnCount; column++) {
+    for (let row = 0; row < brickRowCount; row++) {
+      if (bricks[column][row].status === 1) {
+        let brickX = (column * (brickWidth + brickPadding)) + brickOffsetLeft;
+        let brickY = (row * (brickHeight + brickPadding)) + brickOffsetTop;
 
         bricks[column][row].x = brickX
         bricks[column][row].y = brickY
@@ -46,9 +51,6 @@ function drawBricks() {
         ctx.fillStyle = '#0095DD'
         ctx.fill()
         ctx.closePath()
-      } else if(bricks[column][row].status === 0) {
-        console.log(bricks[column][row])
-        bricks.filter(() => bricks[column][row].status === 1)
       }
     }
   }
@@ -57,7 +59,7 @@ function drawBricks() {
 function drawBall() {
   ctx.beginPath()
   // (положение по оси х, положение по оси у, радиус)
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   // филлстайл хранит цвет
   ctx.fillStyle = '#0095DD'
   // филл - заливает хранимым цветом
@@ -67,27 +69,40 @@ function drawBall() {
 
 function drawPaddle() {
   ctx.beginPath()
-  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight)
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
   ctx.fillStyle = '#0095DD'
   ctx.fill()
   ctx.closePath()
 }
 
 function collisionDetection() {
-  for(let col = 0; col < brickColumnCount; col++) {
-    for(let row = 0; row < brickRowCount; row++) {
+  for (let col = 0; col < brickColumnCount; col++) {
+    for (let row = 0; row < brickRowCount; row++) {
       let brick = bricks[col][row]
 
-      if(x > brick.x && x < brick.x + brickWidth && y - ballRadius > brick.y && y - ballRadius < brick.y + brickHeight) {
+      if (brick.status === 1 && x > brick.x && x < brick.x + brickWidth && y - ballRadius > brick.y && y - ballRadius < brick.y + brickHeight) {
         dy = -dy
         brick.status = 0
-        // bricks.filter(() => bricks[col][row].status === 1)
-        // if(bricks[col][row].status === 0) {
-        //   ctx.clearRect(bricks[col][row].x, bricks[col][row].y, bricks[col][row].x + brickWidth, bricks[col][row].y + brickHeight)
-        // }
+        score++
+        if(score === brickRowCount * brickColumnCount) {
+          alert('YOU ARE AMAZING')
+          document.location.reload()
+        }
       }
     }
   }
+}
+
+function drawScore() {
+  ctx.font = '16px Arial'
+  ctx.fillStyle = '#0095DD'
+  ctx.fillText('Score: ' + score, 8, 20)
+}
+
+function drawLives() {
+  ctx.font = '16px Arial'
+  ctx.fillStyle = '#0095DD'
+  ctx.fillText('Lives: ' + lives, canvas.width - 65, 20)
 }
 
 function draw() {
@@ -101,56 +116,72 @@ function draw() {
   drawBricks()
 
   collisionDetection()
+  drawScore()
+  drawLives()
 
   // меняй положение мяча
   x += dx
   y += dy
 
   // если положение по оси У < 0 или > высоты области рисования, то меняй направление по Y на противоположное
-  if(y < ballRadius) {
+  if (y < ballRadius) {
     dy = -dy
-  } else if(y > canvas.height-ballRadius) {
+  } else if (y > canvas.height - ballRadius) {
     // если ось Х шара больше оси Х левого края ракетки && ось Х шара меньше левого края ракетки + ширина ракетки
-    if(x > paddleX && x < paddleX + paddleWidth) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy
     } else {
-      console.log(bricks)
-      alert('GAME OVER')
-      document.location.reload()
-
+      lives--
+      if(!lives) {
+        alert('GAME OVER')
+        document.location.reload()
+      } else {
+        x = canvas.width/2;
+        y = canvas.height-30;
+        dx = 2;
+        dy = -2;
+        paddleX = (canvas.width-paddleWidth)/2;
+      }
     }
   }
-  if(x < ballRadius || x > canvas.width-ballRadius) {
+  if (x < ballRadius || x > canvas.width - ballRadius) {
     dx = -dx
   }
 
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7
-  } else if(leftPressed && paddleX > 0) {
+  } else if (leftPressed && paddleX > 0) {
     paddleX -= 7
   }
 
-
+  requestAnimationFrame(draw)
 }
 
 document.addEventListener('keydown', keyDownHandler, false)
 document.addEventListener('keyup', keyUpHandler, false)
+document.addEventListener('mousemove', mouseMoveHandler, false)
 
 function keyDownHandler(e) {
-  if(e.keyCode === 39) {
+  if (e.keyCode === 39) {
     rightPressed = true
-  } else if(e.keyCode === 37) {
+  } else if (e.keyCode === 37) {
     leftPressed = true
   }
 }
 
 function keyUpHandler(e) {
-  if(e.keyCode === 39) {
+  if (e.keyCode === 39) {
     rightPressed = false
-  } else if(e.keyCode === 37) {
+  } else if (e.keyCode === 37) {
     leftPressed = false
   }
 }
 
+function mouseMoveHandler(e) {
+  let mouseX = e.clientX - canvas.offsetLeft
+  if(mouseX > 0 && mouseX < canvas.width) {
+    paddleX = mouseX - paddleWidth/2
+  }
+}
 
-let interval = setInterval(draw, 10)
+requestAnimationFrame(draw)
